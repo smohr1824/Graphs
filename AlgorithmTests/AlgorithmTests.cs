d9a43733a757e05b97e2540e3bc6dc77dca66959
Stephen Mohr <smohr@osisoft.com>
2018-08-29 12:47:35 -0400

    Moved bipartite logic to public method taking a starting point; added code to handle the edge case of disconnected vertices.  Aded test coverage.

--- AlgorithmTests/AlgorithmTests.cs
+++ AlgorithmTests/AlgorithmTests.cs
@@ -10,7 +10,7 @@ namespace AlgorithmTests
     public class AlgorithmTests
     {
         [TestMethod]
-        public void BipartiteTest()
+        public void BipartiteTestBasic()
         {
             Network G = new Network(false);
             G.AddVertex("A");
@@ -58,6 +58,52 @@ namespace AlgorithmTests
 
             res = Bipartite.IsBipartite(G, out R, out B);
             Assert.IsTrue(res, "Petri net found not bipartite");
+        }
+
+        [TestMethod]
+        public void BipartiteTestDisconnected()
+        {
+            // create a directed network in which the first two vertices are disconnected, but the network is bipartite
+            Network G = new Network(true);
+            G.AddVertex("A");
+            G.AddVertex("B");
+            G.AddVertex("C");
+            G.AddVertex("D");
+            G.AddVertex("E");
+
+            G.AddEdge("C", "E", 1.0F);
+            G.AddEdge("D", "E", 1.0F);
+
+            List<string> R, B;
+            bool res = Bipartite.IsBipartite(G, out R, out B);
+            Assert.IsTrue(res, "Found bipartite directed network with disconnected vertices to be not bipartite.");
+            Assert.AreEqual(2, R.Count, $"Expected two vertices in the Red list of directed network, found {R.Count}");
+
+            // add an edge making the network not bipartite
+            G.AddEdge("C", "D", 1.0F);
+            res = Bipartite.IsBipartite(G, out R, out B);
+            Assert.IsFalse(res, "Found non-bipartite directed network to be bipartite");
+
+            // repeat for undirected network
+            G = new Network(false);
+            G.AddVertex("A");
+            G.AddVertex("B");
+            G.AddVertex("C");
+            G.AddVertex("D");
+            G.AddVertex("E");
+
+            G.AddEdge("C", "E", 1.0F);
+            G.AddEdge("D", "E", 1.0F);
+
+            res = Bipartite.IsBipartite(G, out R, out B);
+            Assert.IsTrue(res, "Found bipartite undirected network with disconnected vertices to be not bipartite.");
+            Assert.AreEqual(2, R.Count, $"Expected two vertices in the Red list of undirected network, found {R.Count}");
+
+            // add an edge making the network not bipartite
+            G.AddEdge("C", "D", 1.0F);
+            res = Bipartite.IsBipartite(G, out R, out B);
+            Assert.IsFalse(res, "Found non-bipartite undirected network to be bipartite");
+
         }
     }
 }
--- Algorithms/Bipartite.cs
+++ Algorithms/Bipartite.cs
@@ -23,17 +23,43 @@ namespace Networks.Algorithms
             color color;
 
         }
+
+        /// <summary>
+        /// Determines if G is bipartite by attempting a two-coloring of the network.
+        /// </summary>
+        /// <param name="G">network to test</param>
+        /// <param name="Red">if bipartite, list of vertices in one disjoint set</param>
+        /// <param name="Blue">if bipartite, list of vertices in the other disjoint set</param>
+        /// <returns>true if G is bipartite, false otherwise. If bipartite, the two disjoint sets of vertices are returned.</returns>
+        /// <remarks>If G has disconnected vertices, Red.Count() + Blue.Count() will be less than G.Order as the vertices will not be visited and may be included in either disjoint set</remarks>
+        /// 
         public static bool IsBipartite(Network G, out List<string> Red, out List<string> Blue)
+        {
+            string start = G.StartingVertex();
+
+            // check for disconnected vertices
+            if ((G.Directed && G.OutDegree(start) == 0) || (!G.Directed && G.Degree(start) == 0))
+                start = FindConnectedVertex(G);
+
+            return IsBipartite(start, G, out Red, out Blue);
+
+        }
+
+        #region private methods
+        private static bool IsBipartite(string start, Network G, out List<string> Red, out List<string> Blue)
         {
             Red = new List<string>();
             Blue = new List<string>();
 
-            //if (!G.Connected)
-            //    return false;
+            // basic error check, plus traps the edge case of a network with no edges (completely disconnected)
+            // FindConnectedVertex when coming from the overload of this method will return string.Empty in such a case
+            if (start == string.Empty)
+                return false;
+
             Queue<string> tovisit = new Queue<string>();
             Dictionary<string, color> coloring = new Dictionary<string, color>(G.Order);
 
-            string start = G.StartingVertex();
+            // BFS traversal of the network
             coloring[start] = color.red;
             Red.Add(start);
             tovisit.Enqueue(start);
@@ -49,6 +75,20 @@ namespace Networks.Algorithms
                     tocolor = color.red;
 
                 Dictionary<string, float> neighbors = G.GetNeighbors(parent);
+
+                // this is painful, but necessary to ensure reachability in directed graphs
+                // Petri nets do not have this problem, but we are trying for generality here
+                if (G.Directed)
+                {
+                    Dictionary<string, float> predecessors = G.GetSources(parent);
+                    foreach (KeyValuePair<string, float> kvp in predecessors)
+                    {
+                        neighbors.Add(kvp.Key, kvp.Value);
+                    }
+                }
+
+                // check for conflicts in coloring between parent and child
+                // do the coloring otpimistically and compose the lists for the two disjoint sets
                 foreach (string vertex in neighbors.Keys)
                 {
                     color childColor = new color();
@@ -73,8 +113,31 @@ namespace Networks.Algorithms
 
             }
 
-
             return true;
         }
+
+        // Now we're down in the weeds
+        // If a starting point is a disconnected vertex, traverse the list of vertices until a vertex with at least
+        // one adjacent vertex is found.
+        private static string FindConnectedVertex(Network G)
+        {
+            List<string> vertices = G.Vertices;
+
+            string retVal = string.Empty;
+
+            foreach (string vertex in vertices)
+            {
+
+                if ((G.Directed && G.OutDegree(vertex) > 0) || (!G.Directed && G.Degree(vertex) > 0))
+                {
+                    retVal = vertex;
+                    break;
+                }
+            }
+
+            return retVal;
+        }
+
+        #endregion
     }
 }
